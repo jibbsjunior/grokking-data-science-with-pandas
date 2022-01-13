b387066991efb3b074eefc291f4b3edf922b8969
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-13 18:21:51 +0100

    crrating dataframe from array & dictionary

---
+++ pdDataFrame.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+#Dataframe
+df = pd.DataFrame()
+# print('{}\n'.format(df))
+
+df = pd.DataFrame([3, 4])
+# print('{}\n'.format(df))
+
+df = pd.DataFrame([[2, 4]])
+# print('{}'.format(df))
+
+df = pd.DataFrame([[2, 4], [2, 4]], index=['r1', 'r2'], columns=['c1', 'c2'])
+# print('{}\n'.format(df))
+
+df = pd.DataFrame({'c1': [2, 3], 'c2': [4, 5]}, index=['r1', 'r2'])
+# print('{}\n'.format(df))
