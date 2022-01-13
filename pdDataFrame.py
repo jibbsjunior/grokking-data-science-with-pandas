c46f5d982d88aa413568a0206f14a664fced7e96
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-13 18:35:54 +0100

    appending rows to existing dataframe

--- pdDataFrame.py
+++ pdDataFrame.py
@@ -19,5 +19,18 @@ df = pd.DataFrame({'c1': [2, 3], 'c2': [4, 5]}, index=['r1', 'r2'])
 
 #upcasting
 upcast = pd.DataFrame([[2, 4], [3.2, 5]])
-print('{}\n'.format(upcast))
-print("upcast types: \n %s", upcast.dtypes)
+# print('{}\n'.format(upcast))
+# print("upcast types: \n %s", upcast.dtypes)
+
+#appending
+df = pd.DataFrame([[1, 3], [4, 5]])
+ser = pd.Series([2, 1], name='r3')
+
+df_app = df.append(ser)
+# print('{}\n'.format(df_app))
+
+df_app = df.append(ser, ignore_index=True)
+# print('{}\n'.format(df_app))
+df2 = pd.DataFrame([[2, 4], [9, 8]])
+df_app = df.append(df2)
+print('{}\n'.format(df_app))
