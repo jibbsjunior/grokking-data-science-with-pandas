9a972839f59192616e711f3f5906d200407a1d5e
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-13 18:26:39 +0100

    dataframe upcasting

--- pdDataFrame.py
+++ pdDataFrame.py
@@ -16,3 +16,8 @@ df = pd.DataFrame([[2, 4], [2, 4]], index=['r1', 'r2'], columns=['c1', 'c2'])
 
 df = pd.DataFrame({'c1': [2, 3], 'c2': [4, 5]}, index=['r1', 'r2'])
 # print('{}\n'.format(df))
+
+#upcasting
+upcast = pd.DataFrame([[2, 4], [3.2, 5]])
+print('{}\n'.format(upcast))
+print("upcast types: \n %s", upcast.dtypes)
