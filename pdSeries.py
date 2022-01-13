1a10372f6784038ea6899a35d8e2461022447029
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-13 17:22:47 +0100

    working with pd.series func

--- pdSeries.py
+++ pdSeries.py
@@ -1,5 +1,22 @@
 import pandas as pd
 import numpy as np
 
-series = pd.Series()
-print(repr(series))
+#Series
+series = pd.Series(dtype='float64')
+# print('{}'.format(series))
+
+series = pd.Series(5)
+# print('{}\n'.format(series))
+
+series = pd.Series([1, 2, 3])
+# print('{}\n'.format(series))
+
+series = pd.Series([2, 2.5]) #upcasting
+# print('{}\n'.format(series))
+
+arr = np.array([1, 3, 4])
+series = pd.Series(arr, dtype=np.float32)
+# print('{}\n'.format(series))
+
+series = pd.Series([[1, 2], [2, 3]])
+print('{}\n'.format(series))
