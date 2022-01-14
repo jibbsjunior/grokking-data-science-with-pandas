07f10365639498e9c94bef713d22cbfdedac1b86
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-14 13:47:40 +0100

    Quiz completed for data indexing

--- pdIndexing.py
+++ pdIndexing.py
@@ -35,4 +35,18 @@ single_val = df.loc['r1', 'c2']
 df.loc[['r1', 'r3'], 'c2'] = 0
 # print('{}\n'.format(df))
 
+#Quiz
+# 1. The coding exercises for this chapter involve directly indexing into a predefined DataFrame, df.
+# We'll initially use direct indexing to get the first column of df as well as the first two rows.
+col_1 = df['c1']
+row_12 = df[0:2]
+
+# 2. Next, we'll use iloc to retrieve the first and third rows of df.
+# CODE HERE
+row_13 = df.iloc[[0, 2]]
+
+# 3. Finally, we use loc to set each value of the second column, in the third and fourth rows, equal to 12. The row key we use for indexing will be ['r3','r4'], while the column key will be 'c2'.
+# CODE HERE
+df.loc[['r3', 'r4'], 'c2'] = 12
+
 
