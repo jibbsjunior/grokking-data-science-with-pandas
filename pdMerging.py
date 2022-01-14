b50b7283c844e5b65ee5367f1462afefb3264973
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-14 11:15:18 +0100

    working with pd.merge func

--- pdMerging.py
+++ pdMerging.py
@@ -13,4 +13,16 @@ concat = pd.concat([df1, df2], axis=1)
 concat = pd.concat([df1, df2, df3])
 
 concat = pd.concat([df3, df1], axis=1)
-print('{}\n'.format(concat))
+# print('{}\n'.format(concat))
+
+#Merging
+mlb_df1 = pd.DataFrame({'name': ['john doe', 'al smith', 'sam black', 'john doe'],
+                        'pos': ['1B', 'C', 'P', '2B'],
+                        'year': [2000, 2004, 2008, 2003]})
+mlb_df2 = pd.DataFrame({'name': ['john doe', 'al smith', 'jack lee'],
+                        'year': [2000, 2004, 2012],
+                        'rbi': [80, 100, 12]})
+# print('{}\n'.format(mlb_df1))
+# print('{}\n'.format(mlb_df1))
+mlb_merged = pd.merge(mlb_df1, mlb_df2)
+print('{}\n'.format(mlb_merged))
