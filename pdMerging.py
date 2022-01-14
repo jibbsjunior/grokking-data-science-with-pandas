fbcbdd7f43abd23cd3700d89d674411e1119c620
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-14 11:25:04 +0100

    Quiz completed for pd.merge and pd.concat

--- pdMerging.py
+++ pdMerging.py
@@ -26,3 +26,30 @@ mlb_df2 = pd.DataFrame({'name': ['john doe', 'al smith', 'jack lee'],
 # print('{}\n'.format(mlb_df1))
 mlb_merged = pd.merge(mlb_df1, mlb_df2)
 print('{}\n'.format(mlb_merged))
+
+#Quiz
+# 1. The coding exercises for this chapter involve completing small functions that take in two DataFrame objects as input.
+# The first function, concat_rows will concatenate the rows of the two DataFrames.
+def concat_rows(df1, df2):
+  # CODE HERE
+  row_concat = pd.concat(df1, df2)
+
+  return row_concat
+  pass
+
+# 2. The next function, concat_cols will concatenate the columns of the two input DataFrames.
+def concat_cols(df1, df2):
+  # CODE HERE
+  col_concat = pd.concat([df1, df2], axis=1)
+
+  return col_concat
+  pass
+
+# 3. The final function, merge_dfs will merge the two input DataFrames along their columns.
+def merge_dfs(df1, df2):
+  # CODE HERE
+  merged_df = pd.merge([df1, df2])
+
+  return merged_df
+  pass
+
