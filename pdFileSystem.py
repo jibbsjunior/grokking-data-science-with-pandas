7d998b8672af8e3dcd258a8d82b421af47d01d67
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-15 13:14:52 +0100

    reading from a csv file

--- pdFileSystem.py
+++ pdFileSystem.py
@@ -1,5 +1,10 @@
+from operator import index
 import pandas as pd
 
 #csv
-df = pd.read_csv('dummy.csv')
+df = pd.read_csv('./pandas/dummy.csv')
+
+df = pd.read_csv('./pandas/dummy.csv', index_col=0)
+
+df = pd.read_csv('./pandas/dummy.csv', index_col=1)
 print('{}\n'.format(df))
