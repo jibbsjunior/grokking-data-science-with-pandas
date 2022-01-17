2c3216cc2cb2097100d9b3dded56da4c2152b2a3
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-17 15:33:31 +0100

    working with dataframe.tocsv func

--- pdFileSystem.py
+++ pdFileSystem.py
@@ -2,17 +2,27 @@ from operator import index
 import pandas as pd
 
 #csv
-df = pd.read_csv('./pandas/dummy.csv')
+# df = pd.read_csv('./pandas/dummy.csv')
 
-df = pd.read_csv('./pandas/dummy.csv', index_col=0)
+# df = pd.read_csv('./pandas/dummy.csv', index_col=0)
 
-df = pd.read_csv('./pandas/dummy.csv', index_col=1)
+# df = pd.read_csv('./pandas/dummy.csv', index_col=1)
+# # print('{}\n'.format(df))
+
+# #excel
+# df = pd.read_excel('./pandas/excel_dummy.xlsx')
+
+# df = pd.read_excel('./pandas/excel_dummy.xlsx', sheet_name=None)
+# # print('{}\n'.format(df))
+
+# #json
+# df = pd.read_json('./pandas/dummy.json')
+
+# df = pd.read_json('./pandas/dummy.json', orient=index)
 # print('{}\n'.format(df))
 
-#excel
-df = pd.read_excel('./pandas/excel_dummy.xlsx')
 
-df = pd.read_excel('./pandas/excel_dummy.xlsx', sheet_name=None)
-# print('{}\n'.format(df))
-
-#json
+#writing to files - csv
+df = pd.DataFrame([['Adebayo AJibola', 'Adegbenro Mariam', 'Stephen Akpovino'], ['Lanre Kuye', 'Big Abbass']])
+df.to_csv()
+print('{}\n'.format(df))
