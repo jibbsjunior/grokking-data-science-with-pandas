a081ede57e45dbb502051a0fe2816394927cd8fd
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2022-01-18 15:05:21 +0100

    Quiz completed for data sorting

--- pdMetrics.py
+++ pdMetrics.py
@@ -31,5 +31,20 @@ unique_teams = df['teamID'].unique()
 y_ids = df['yearID']
 # print('{}\n'.format(y_ids))
 # print('{}\n'.format(repr(y_ids.unique())))
-print('{}\n'.format(y_ids.value_counts()))
+# print('{}\n'.format(y_ids.value_counts()))
+
+#Quiz
+# 1. First, we'll get a summary of all the statistics in player_df.
+# CODE HERE
+# summary_all = player_df.describe()
+
+# 2. Next, we want to get summaries specifically for the home run totals. The first summary will contain the default metrics from describe, while the second summary will contain the 10th and 90th percentiles.
+# CODE HERE
+# hr_df = player_df['HR']
+# summary_hr = hr_df.describe()
+# low_high_10 = hr_df.describe([.1, .9])
+
+# 3. Finally, we'll treat the 'HR' feature as a categorical variable, with each unique home run total as a separate category. We then get the frequency counts for each category.
+# CODE HERE
+# hr_counts = hr_df.value_counts()
 
